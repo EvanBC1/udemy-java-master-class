97042d08c808183173ccd59d30c1643cf5fbabb8
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-04-10 15:58:02 -0700

    28 done

---
+++ 28. float/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	     float myMinFloat = Float.MIN_VALUE;
+//	     float myMaxFloat = Float.MAX_VALUE;
+//
+//	     System.out.println(myMinFloat);
+//	     System.out.println(myMaxFloat);
+//
+//	     double myMinDouble = Double.MIN_VALUE;
+//	     double myMaxDouble = Double.MAX_VALUE;
+//
+//	     System.out.println(myMinDouble);
+//	     System.out.println(myMaxDouble);
+//
+//	     int myIntValue = 5 / 3;
+//	     float myFloat = 5f / 3f;
+//	     double myDouble = 5d / 3d;
+//
+//	     System.out.println(myIntValue);
+//	     System.out.println(myFloat);
+//	     System.out.println(myDouble);
+
+		 int lbs = 200;
+		 double lbsToKgs =.45359237;
+		 System.out.println(lbs * lbsToKgs);
+    }
+}
