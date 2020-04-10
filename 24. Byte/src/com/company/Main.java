02100406475219aab8ed11c2f60009197ec59369
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-04-10 11:00:05 -0700

    25 done

--- 24. Byte/src/com/company/Main.java
+++ 24. Byte/src/com/company/Main.java
@@ -14,6 +14,19 @@ public class Main {
         System.out.println("Busted max Value = " + (myMaxIntValue + 1));
         System.out.println("Busted min Value = " + (myMinIntValue - 1));
 
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+
+        System.out.println(myMaxlongValue );
+        System.out.println(myMinlongValue );
+
+        long myLongValue = 100L;
+
+        System.out.println(myLongValue);
+
+        long bigLong = 2_147_483_647_234L;
+
+        short bigShort = 32767;
 
     }
 }
