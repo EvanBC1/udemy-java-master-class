a1cd588709e75407fbb0e8547d6f2c3cb082690d
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-04-28 08:10:26 -0700

    finished leap year

--- 50. diffMerge/src/com/company/Main.java
+++ 50. diffMerge/src/com/company/Main.java
@@ -2,21 +2,32 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        shouldWakeUp(true, 1);
-        shouldWakeUp(false, 2);
-        shouldWakeUp(true, 8);
-        shouldWakeUp(true, -1);
+    isLeapYear(-1600);
+    isLeapYear(1600);
+    isLeapYear(2017);
+    isLeapYear(2000);
     }
-    public static boolean shouldWakeUp(boolean barking, int hourOfTheDay) {
-        if (hourOfTheDay < 0 || hourOfTheDay > 23) {
-            System.out.println("false");
+    public static boolean isLeapYear(int year) {
+        if(year <= 0 || year >= 10000) {
+            System.out.println(false);
             return false;
         }
-        if ((hourOfTheDay < 8 || hourOfTheDay > 22) && barking) {
-            System.out.println("true");
-            return true;
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    System.out.println(true);
+                    return true;
+                } else {
+                    System.out.println(false);
+                    return false;
+                }
+            } else {
+                System.out.println(true);
+                return true;
+            }
+        } else {
+            System.out.println(false);
+            return false;
         }
-        System.out.println("false");
-        return false;
     }
 }
