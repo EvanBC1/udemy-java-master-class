11f0c719e8b65adda59542bce1ba56c7c0e1a183
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-05-14 15:27:26 -0700

    finished 6

--- 50. diffMerge/src/com/company/Main.java
+++ 50. diffMerge/src/com/company/Main.java
@@ -2,23 +2,16 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        areEqualByThreeDecimalPlaces(-3.1756, -3.1755);
-        areEqualByThreeDecimalPlaces(3.175, 3.176);
-        areEqualByThreeDecimalPlaces(3.0, 3.0);
-        areEqualByThreeDecimalPlaces(-3.123, 3.123);
+        hasEqualSum(1, 1, 1);
+        hasEqualSum(1,1,2);
+        hasEqualSum(1,-1,2);
     }
-    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
-       num1 = num1 * 1000;
-       num2 = num2 * 1000;
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
 
-       num1 = (int) num1;
-       num2 = (int) num2;
+       if (num1 + num2 == num3) {
+          return true;
+       }
 
-        if(num1 == num2) {
-            System.out.println(true);
-            return true;
-        }
-        System.out.println(false);
-        return false;
+       return false;
     }
 }
