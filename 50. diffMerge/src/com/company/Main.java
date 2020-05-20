a0bab9ab0779dcd50d0cb24d4baf979751a9cd93
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-05-20 13:49:37 -0700

    ce 27 done

--- 50. diffMerge/src/com/company/Main.java
+++ 50. diffMerge/src/com/company/Main.java
@@ -3,25 +3,31 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-      System.out.println("This program will to 10 numbers that you enter and then return the sum of those numbers");
-       Scanner sc = new Scanner(System.in);
-       int sum = 0;
-      for(int i = 0; i < 3; i++) {
-         System.out.println("Enter integer " + (i + 1) + " below:");
-         if(sc.hasNextInt()) {
-            sum += sc.nextInt();
-         } else {
-            System.out.println("Invalid input");
-            i--;
+   public static void main(String[] args) {
+      inputThenPrintSumAndAverage();
+   }
+
+   public static void inputThenPrintSumAndAverage() {
+
+      Scanner sc = new Scanner(System.in);
+      boolean numbersOnly = true;
+      int sum = 0;
+      long average = 0;
+      int currentInt = 0;
+      int count = 0;
+
+      while (numbersOnly) {
+         if (!sc.hasNextInt()) {
+            System.out.println("SUM = " + sum + " AVG = " + average);
+            return;
          }
-         sc.nextLine();
+
+         count++;
+         currentInt = sc.nextInt();
+         sum += currentInt;
+
+         average = Math.round((double) sum / (double) count);
       }
-      System.out.println(sum);
-
       sc.close();
-
-
-    }
-
+   }
 }
